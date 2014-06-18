8a6aa3c49bfecb698fc33985fd5bd9a7531088b5
bwstaal <b.w.staal@gmail.com>
2014-06-18 17:33:16 +0200

    Added code from the MSDN article

--- SaveUMLDiagramToImageFileCommandExtension/CommandExtension.cs
+++ SaveUMLDiagramToImageFileCommandExtension/CommandExtension.cs
@@ -1,54 +1,108 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.ComponentModel.Composition;
-using Microsoft.VisualStudio.ArchitectureTools.Extensibility.Presentation;
-using Microsoft.VisualStudio.ArchitectureTools.Extensibility.Uml;
+﻿using System.ComponentModel.Composition; // for [Import], [Export]
+using System.Drawing; // for Bitmap
+using System.Drawing.Imaging; // for ImageFormat
+using System.Linq; // for collection extensions
+using System.Windows.Forms; // for SaveFileDialog
+using Microsoft.VisualStudio.Modeling.Diagrams;
+// for Diagram
 using Microsoft.VisualStudio.Modeling.ExtensionEnablement;
-using Microsoft.VisualStudio.Uml.Classes;
+// for IGestureExtension, ICommandExtension, ILinkedUndoContext
+using Microsoft.VisualStudio.ArchitectureTools.Extensibility.Presentation;
+// for IDiagramContext
+using Microsoft.VisualStudio.ArchitectureTools.Extensibility.Uml;
+// for designer extension attributes
 
 namespace SaveUMLDiagramToImageFileCommandExtension
 {
-    // Custom context menu command extension
-    // See http://msdn.microsoft.com/en-us/library/ee329481(VS.110).aspx
+    /// <summary>
+    /// Called when the user clicks the menu item.
+    /// </summary>
+    // Context menu command applicable to any UML diagram 
     [Export(typeof(ICommandExtension))]
-    [ClassDesignerExtension] // TODO: Add other diagram types if needed
+    [ClassDesignerExtension]
+    [UseCaseDesignerExtension]
+    [SequenceDesignerExtension]
+    [ComponentDesignerExtension]
+    [ActivityDesignerExtension]
     class CommandExtension : ICommandExtension
     {
         [Import]
-        IDiagramContext context { get; set; }
-
+        IDiagramContext Context { get; set; }
 
         public void Execute(IMenuCommand command)
         {
-            // TODO: Add the logic for your command extension here
-
-            // The following example creates a new class in the model store
-            // and displays it on the current diagram.
-            IClassDiagram diagram = context.CurrentDiagram as IClassDiagram;
-            IModelStore store = diagram.ModelStore;
-            IPackage rootPackage = store.Root;
-            IClass newClass = rootPackage.CreateClass();
-            newClass.Name = "SaveUMLDiagramToImageFileCommandExtension";
-            diagram.Display(newClass);
+            // Get the diagram of the underlying implementation.
+            Diagram dslDiagram = Context.CurrentDiagram.GetObject<Diagram>();
+            if (dslDiagram != null)
+            {
+                string imageFileName = FileNameFromUser();
+                if (!string.IsNullOrEmpty(imageFileName))
+                {
+                    Bitmap bitmap = dslDiagram.CreateBitmap(
+                     dslDiagram.NestedChildShapes,
+                     Diagram.CreateBitmapPreference.FavorClarityOverSmallSize);
+                    bitmap.Save(imageFileName, GetImageType(imageFileName));
+                }
+            }
         }
 
+        /// <summary>
+        /// Called when the user right-clicks the diagram.
+        /// Set Enabled and Visible to specify the menu item status.
+        /// </summary>
+        /// <param name="command"></param>
         public void QueryStatus(IMenuCommand command)
         {
-            // TODO: Add logic to control the display of your menu item
-
-            // The following example will disable the command extension unless the user selected
-            // a class shape.
-            //
-            //   IShape selshape = context.CurrentDiagram.SelectedShapes.FirstOrDefault();
-            //   command.Enabled = selshape.Element is IClass;
-            //
-            // Note: Setting command.Visible=false can have unintended interactions with other extensions.
+            command.Enabled = Context.CurrentDiagram != null
+              && Context.CurrentDiagram.ChildShapes.Count() > 0;
         }
 
+        /// <summary>
+        /// Menu text.
+        /// </summary>
         public string Text
         {
-            get { return "SaveUMLDiagramToImageFileCommandExtension"; }
+            get { return "Save To Image..."; }
+        }
+
+
+        /// <summary>
+        /// Ask the user for the path of an image file.
+        /// </summary>
+        /// <returns>image file path, or null</returns>
+        private string FileNameFromUser()
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.AddExtension = true;
+            dialog.DefaultExt = "image.bmp";
+            dialog.Filter = "Bitmap ( *.bmp )|*.bmp|JPEG File ( *.jpg )|*.jpg|Enhanced Metafile (*.emf )|*.emf|Portable Network Graphic ( *.png )|*.png";
+            dialog.FilterIndex = 1;
+            dialog.Title = "Save Diagram to Image";
+            return dialog.ShowDialog() == DialogResult.OK ? dialog.FileName : null;
+        }
+
+        /// <summary>
+        /// Return the appropriate image type for a file extension.
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        private ImageFormat GetImageType(string fileName)
+        {
+            string extension = System.IO.Path.GetExtension(fileName).ToLowerInvariant();
+            ImageFormat result = ImageFormat.Bmp;
+            switch (extension)
+            {
+                case ".jpg":
+                    result = ImageFormat.Jpeg;
+                    break;
+                case ".emf":
+                    result = ImageFormat.Emf;
+                    break;
+                case ".png":
+                    result = ImageFormat.Png;
+                    break;
+            }
+            return result;
         }
     }
 }
